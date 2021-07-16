45d9fdb08c8077df047a27afdf8e159e7c6eec5f
MahmoudElmahalawy <elmehalawy7@gmail.com>
2021-07-16 21:31:48 +0200

    Finished login and home screens

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,39 +1,61 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {View, Text, TouchableOpacity, Image, FlatList} from 'react-native';
 import {styles} from './styles';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-export default function Home() {
-  const DATA = [
-    {
-      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-      source: require('../../assets/images/1.png'),
-    },
-    {
-      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-      source: require('../../assets/images/2.png'),
-    },
-    {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      source: require('../../assets/images/3.png'),
-    },
-  ];
+const DATA = [
+  {
+    id: '1',
+    source: require('../../assets/images/1.png'),
+  },
+  {
+    id: '2',
+    source: require('../../assets/images/2.png'),
+  },
+  {
+    id: '3',
+    source: require('../../assets/images/3.png'),
+  },
+  {
+    id: '4',
+    source: require('../../assets/images/4.png'),
+  },
+  {
+    id: '5',
+    source: require('../../assets/images/5.png'),
+  },
+  {
+    id: '6',
+    source: require('../../assets/images/6.png'),
+  },
+  {
+    id: '7',
+    source: require('../../assets/images/7.png'),
+  },
+  {
+    id: '8',
+    source: require('../../assets/images/8.png'),
+  },
+];
 
-  const Item = ({source}) => (
-    <View style={styles.imageContainer}>
-      <Image style={styles.image} source={source} />
-    </View>
-  );
+const Item = ({source}) => (
+  <View style={styles.imageContainer}>
+    <Image style={styles.image} source={source} />
+  </View>
+);
 
+export default function Home({navigation}) {
   const renderItem = ({item}) => <Item source={item.source} />;
-
+  const handleBackBtn = () => {
+    navigation.goBack();
+  };
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity style={styles.backBtn}>
+        <TouchableOpacity style={styles.backBtn} onPress={handleBackBtn}>
           <Icon name="chevron-left" size={20} color="#DDD" />
         </TouchableOpacity>
-        <Text style={styles.title}>Gallery</Text>
+        <Text style={styles.title}>Home</Text>
       </View>
       <FlatList data={DATA} renderItem={renderItem} />
     </View>
